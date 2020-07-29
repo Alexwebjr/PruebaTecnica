518f3794d66d4facdbddaa6cc6b73311153fd1d2
Alejandro Cortorreal <alejandrocortorreal18@outlook.com>
2020-07-29 10:17:27 -0400

    FrontEnd

--- Frontend/Controllers/HomeController.cs
+++ Frontend/Controllers/HomeController.cs
@@ -7,24 +7,28 @@ using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using Frontend.Models;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace Frontend.Controllers
 {
     public class HomeController : Controller
     {
-        public async Task<ActionResult> Index(int id=0)
+        [HttpGet]
+        public async Task<ActionResult> Index(int? id)
         {
-            if (id != 0)
+            if (id != null)
             {
-                string apiUrl = "https://localhost:44318/api/books/" + id;
-                var httpClient = new HttpClient();
-                var bookjson = await httpClient.GetStringAsync(apiUrl);
-                dynamic objc = JObject.Parse(bookjson);
+                string apiUrl2 = "https://localhost:44318/api/books/" + id;
+                var httpCliente = new HttpClient();
+                var bookjs = await httpCliente.GetStringAsync(apiUrl2);
+                dynamic objc = JObject.Parse(bookjs);
                 Book Modelo = objc.ToObject<Book>();
                 return View(Modelo);
             }
-            else
-            {
+            else{
+
+            }
                 string apiUrl = "https://localhost:44318/api/books";
                 var httpClient = new HttpClient();
                 var bookjson = await httpClient.GetStringAsync(apiUrl);
@@ -32,33 +36,54 @@ namespace Frontend.Controllers
                 IList<Book> BooksList = arrayBook.ToObject<IList<Book>>();
                 ViewBag.BooksList = BooksList;
                 return View();
-            }
-            
         }
+
         
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Index(int id, Book Libro)
+        public async Task<ActionResult> Index(Book Libro)
         {
-            string apiUrl = "https://localhost:44318/api/books/" + id;
+            //Set DateTime
+            DateTime Publicado = Convert.ToDateTime(Libro.PublishDate);
+            Libro.PublishDate = Publicado.ToString("yyyy-MM-ddTHH:mm:ss");
+
+            string apiUrl = "https://localhost:44318/api/books";
             var httpClient = new HttpClient();
-            var bookjson = await httpClient.GetStringAsync(apiUrl);
-            dynamic objc = JObject.Parse(bookjson);
+
+            //Serializar
+            var json = JsonConvert.SerializeObject(Libro);
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var bookjson = await httpClient.PostAsync(apiUrl, data);
+            string result = bookjson.Content.ReadAsStringAsync().Result;
+
+            dynamic objc = JObject.Parse(result);
             Book Modelo = objc.ToObject<Book>();
             return View(Modelo);
         }
 
-
+        //Edit
+        [HttpGet]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(int id)
+        {
+                string apiUrl = "https://localhost:44318/api/books/" + id;
+                var httpClient = new HttpClient();
+                var bookjson = await httpClient.GetStringAsync(apiUrl);
+                dynamic objc = JObject.Parse(bookjson);
+                Book Modelo = objc.ToObject<Book>();
+                return View(Modelo);
+        }
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Edit(int id, Book Libro)
+        public async Task<ActionResult> Edit(Book Libro)
         {
-            string apiUrl = "https://localhost:44318/api/books/" + id;
-            var httpClient = new HttpClient();
-            var bookjson = await httpClient.GetStringAsync(apiUrl);
-            dynamic objc = JObject.Parse(bookjson);
-            Book Modelo = objc.ToObject<Book>();
-            return View(Modelo);
+                string apiUrl = "https://localhost:44318/api/books/" + Libro.ID;
+                var httpClient = new HttpClient();
+                var bookjson = await httpClient.GetStringAsync(apiUrl);
+                dynamic objc = JObject.Parse(bookjson);
+                Book Modelo = objc.ToObject<Book>();
+                return View(Modelo);
         }
 
         public ActionResult About()
--- PruebaTecnica/Models/Book.cs
+++ PruebaTecnica/Models/Book.cs
@@ -12,6 +12,6 @@ namespace PruebaTecnica.Models
         public string Description { get; set; }
         public int PageCount { get; set; }
         public string Excerpt { get; set; }
-        public DateTime PublishDate { get; set; }
+        public string PublishDate { get; set; }
     }
 }
